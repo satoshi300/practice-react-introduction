c1cf635c7f1349ab6e96c548d7d18e6d50dc5a70
Michal Wasiak <michal.wasiak@hotmail.com>
2025-12-02 16:33:24 +1000

    Add styles

--- 02/app.js
+++ 02/app.js
@@ -3,21 +3,29 @@ import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.querySelector('#root'));
 
+const headerStyles = {
+    border: '1px solid blue',
+    color: 'red',
+    backgroundColor: 'yellow',
+    padding: '20px',
+    textAlign:'center'
+}
 const HeaderFn = () => {
     return (
-        <header>Moja pierwsza strona w React.</header>
+        <header style={headerStyles}>Moja pierwsza strona w React.</header>
     );
 };
 
 class HeaderCl extends React.Component {
     render() {
-        return <header>Moja pierwsza strona w React!</header>
+        return <header style={headerStyles}>Moja pierwsza strona w React!</header>
     }
 }
 
 const App = () => {
     return (
         <>
+
             <HeaderFn />
             <HeaderCl />
         </>
